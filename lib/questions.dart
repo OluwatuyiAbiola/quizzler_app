ebf0e2f8d5444fdeb39390246eb6d4a874f73b3e
Oluwatuyi Abiola <abiolaoluwatuyi@gmail.com>
2022-06-17 00:07:42 +0100

    made changes to question.dart

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,12 +1,9 @@
 class Question {
-  String questionText = 'This is a text';
-  bool questionAnswer = true;
+  String questionText;
+  bool questionAnswer;
 
 
-  Question(String s, bool a){
-    questionText = s;
-    questionAnswer = a;
-  }
+  Question(this.questionText, this.questionAnswer);
 }
 
 Question newQuestion = Question( 'text', true);
